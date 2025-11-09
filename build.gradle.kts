bab2fa8c02ce9da649444e7c80ba17badf6fafed
Petio <natzkinp@gmail.com>
2025-11-09 16:26:00 +0100

    implemented page link redirection!

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Minimal Gradle configuration for GitHub Pages website module (dev)
+// This allows the folder to be visible in Android Studio project tree
+
+// No actual build tasks needed - this is just static HTML/JSON files
+// that are deployed to GitHub Pages
